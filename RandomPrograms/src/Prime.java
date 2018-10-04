8319d3bcb8a4b3f67786c08b1beac984d1e79a5e
Abhishek Sen <abhishek.mcs18.du@gmail.com>
2018-10-05 01:19:30 +0530

    added program

---
+++ RandomPrograms/src/Prime.java
@@ -0,0 +1,32 @@
+import java.io.*;
+class Prime 
+{
+	static int count;
+	static int isPrime(int n) 
+	{
+		if (n<=1)return 0;
+		else if (n==2)return 1;
+		int i=2;
+		int cn=n;
+		for(i=2;i<cn;i++)
+		{
+			count++;
+			if(n%i==0)return 0;
+			
+			cn=n/i;
+		}
+		return 1;
+	}
+	
+	public static void main(String args[]) throws IOException
+	{
+		BufferedReader buf=new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("enter the number: ");
+		int n=Integer.parseInt(buf.readLine());
+		count=0;
+		int check=isPrime(n);
+		System.out.println(n + "   isPrime> "+(check==1?"true":"false")+"   number of modulo checks: "+count);
+		
+	}
+	
+}
